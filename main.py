b02da198c2baf518d192bee55537f030cb62e50c
Konstantin Rezanov <kostarezanov@gmail.com>
2023-06-03 23:35:33 +0300

    commit 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
 from telebot import types
+from telebot.types import ReplyKeyboardMarkup
 
 bot = telebot.TeleBot('6013348559:AAF1zPtGlKAeEWSAvzI3KicRG6CjpAN1Hxo', parse_mode=None)
 
@@ -7,6 +8,16 @@ bot = telebot.TeleBot('6013348559:AAF1zPtGlKAeEWSAvzI3KicRG6CjpAN1Hxo', parse_mo
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message: types.Message):
     bot.send_message(message.chat.id, 'Этот бот предоставляет регулярные отчеты о вашем портфеле на Московской Бирже')
+
+
+@bot.message_handler(commands=['update_tickers'])
+def update_ticker(message: types.Message):
+    markup: ReplyKeyboardMarkup = types.ReplyKeyboardMarkup(row_width=2)
+    button1 = types.KeyboardButton("Тинькофф")
+    button2 = types.KeyboardButton("БКС")
+    button3 = types.KeyboardButton("Другой брокер/загрузить собственный список")
+    markup.add(button1, button2, button3)
+    bot.send_message(message.chat.id, "Выберете брокера из которого хотите загрузить данные:", reply_markup=markup)
 
 
 def main():
--- report.py
+++ report.py
@@ -1,3 +1,18 @@
 import pandas as pd
 
-# def get_report(exchange_data: pd.DataFrame) -> str:
+
+def load_demo_report(demo_path: str) -> pd.DataFrame:
+    return pd.read_csv(demo_path, sep=";", index_col='ticker')
+
+
+def format_ticker(ticker_info: pd.Series) -> str:
+    return f"{ticker_info.name}: {ticker_info['close']}"
+
+
+def format_report(exchange_data: pd.DataFrame) -> str:
+    report_lines = [format_ticker(exchange_data.loc[ticker]) for ticker in exchange_data.index]
+    return "\n".join(report_lines)
+
+
+data = load_demo_report('demo.csv')
+print(format_report(data))
