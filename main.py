104f603dde93bea0d14accf885f28b767b8d7435
Konstantin Rezanov <kostarezanov@gmail.com>
2023-06-04 09:30:22 +0300

    Added requirements.txt

--- main.py
+++ main.py
@@ -13,11 +13,21 @@ def send_welcome(message: types.Message):
 @bot.message_handler(commands=['update_tickers'])
 def update_ticker(message: types.Message):
     markup: ReplyKeyboardMarkup = types.ReplyKeyboardMarkup(row_width=2)
-    button1 = types.KeyboardButton("Тинькофф")
-    button2 = types.KeyboardButton("БКС")
-    button3 = types.KeyboardButton("Другой брокер/загрузить собственный список")
-    markup.add(button1, button2, button3)
-    bot.send_message(message.chat.id, "Выберете брокера из которого хотите загрузить данные:", reply_markup=markup)
+    markup.add("Тинькофф", "БКС", "Другой брокер/загрузить собственный список")
+    msg = bot.send_message(message.chat.id, "Выберете брокера из которого хотите загрузить данные:",
+                           reply_markup=markup)
+    bot.register_next_step_handler(msg, handle_broker_choice)
+
+
+def handle_broker_choice(message: types.Message):
+    if message.text == "Тинькофф":
+        bot.send_message(message.chat.id, "Отправьте Excel-файл с отчетом")
+    elif message.text == "БКС":
+        bot.send_message(message.chat.id, "Отправьте Excel-файл с отчетом")
+    elif message.text == "Другой брокер/загрузить собственный список":
+        bot.send_message(message.chat.id, "Загрузить Excel-файл с колонками ticker, type. Подробнее: ")
+    else:
+        bot.send_message(message.chat.id, "Выбран неверный вариант")
 
 
 def main():
