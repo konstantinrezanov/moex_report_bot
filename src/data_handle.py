08ce6d27d880fac7a7c337b79382c32510bab502
Konstantin Rezanov <kostarezanov@gmail.com>
2023-06-16 00:00:49 +0300

    Added doctrings Added caching Started work on jupyter description

--- src/data_handle.py
+++ src/data_handle.py
@@ -4,45 +4,113 @@ import os
 
 
 def setup_db(db_path: str):
+    """
+    Creates the directory for the database file if it doesn't exist.
+
+    Args:
+        db_path (str): The path to the database file.
+    """
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
 
 
 def store_tickers(db_path: str, chat_id: int, tickers: pd.Series):
-    db_path = TinyDB(db_path)
-    User = Query()
+    """
+    Stores the tickers associated with a chat ID in the database.
+
+    Args:
+        db_path (str): The path to the database file.
+        chat_id (int): The ID of the chat.
+        tickers (pd.Series): A Pandas Series object containing the tickers.
+
+    """
+    db_path = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
+    User = Query()  # Create a Query object for performing queries on the database
     db_path.upsert({'chat_id': chat_id, "tickers": list(tickers.values)}, User.chat_id == chat_id)
+    # Update or insert a new document in the database with chat_id and tickers
 
 
 def store_time(db_path: str, chat_id: int, time_choice: tuple):
-    db_path = TinyDB(db_path)
-    User = Query()
+    """
+    Stores the time choice associated with a chat ID in the database.
+
+    Args:
+        db_path (str): The path to the database file.
+        chat_id (int): The ID of the chat.
+        time_choice (tuple): A tuple representing the time choice.
+
+    """
+    db_path = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
+    User = Query()  # Create a Query object for performing queries on the database
     db_path.upsert({'chat_id': chat_id, "time": time_choice}, User.chat_id == chat_id)
+    # Update or insert a new document in the database with chat_id and time_choice
 
 
 def get_user_tickers(db_path: str, chat_id: int) -> list:
-    db = TinyDB(db_path)
-    chat = Query()
+    """
+    Retrieves the tickers associated with a chat ID from the database.
+
+    Args:
+        db_path (str): The path to the database file.
+        chat_id (int): The ID of the chat.
+
+    Returns:
+        list: A list of tickers associated with the chat ID.
+    """
+    db = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
+    chat = Query()  # Create a Query object for performing queries on the database
     return db.get(chat.chat_id == chat_id)['tickers']
+    # Retrieve the tickers field from the document with the matching chat_id
 
 
 def set_jobs_dict(db_path: str) -> dict:
-    db = TinyDB(db_path)
-    jobs = {}
+    """
+    Creates a dictionary of chat IDs and their corresponding time choices from the database.
+
+    Args:
+        db_path (str): The path to the database file.
+
+    Returns:
+        dict: A dictionary mapping chat IDs to time choices.
+    """
+    db = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
+    jobs = {}  # Dictionary to store the chat IDs and time choices
     for user in db.all():
         try:
             jobs[user["chat_id"]] = tuple(user["time"])
+            # Add chat ID and time choice as a key-value pair to the jobs dictionary
         except KeyError:
-            continue
+            continue  # Skip users without a time choice field
 
     return jobs
 
 
 def get_user_time(db_path: str, chat_id: int) -> tuple:
-    db = TinyDB(db_path)
-    chat = Query()
+    """
+    Retrieves the time choice associated with a chat ID from the database.
+
+    Args:
+        db_path (str): The path to the database file.
+        chat_id (int): The ID of the chat.
+
+    Returns:
+        tuple: The time choice associated with the chat ID.
+    """
+    db = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
+    chat = Query()  # Create a Query object for performing queries on the database
     return db.get(chat.chat_id == chat_id)['time']
+    # Retrieve the time field from the document with the matching chat_id
 
 
 def get_users(db_path: str) -> list:
-    db = TinyDB(db_path)
+    """
+    Retrieves a list of chat IDs from the database.
+
+    Args:
+        db_path (str): The path to the database file.
+
+    Returns:
+        list: A list of chat IDs.
+    """
+    db = TinyDB(db_path)  # Initialize the TinyDB object with the given database path
     return [x['chat_id'] for x in db.all()]
+    # Extract the chat_id field from all documents in the database and return as a list
--- src/ticker_update.py
+++ src/ticker_update.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import data_handle
 
+
 def excel_update(file_path: str, db_path: str, chat_id: int):
+    """
+    Updates the database with tickers from an Excel file.
+
+    Args:
+        file_path (str): The path to the Excel file.
+        db_path (str): The path to the database file.
+        chat_id (int): The chat ID associated with the tickers.
+
+    Returns:
+        None
+    """
     tickers = pd.read_excel(file_path)['ticker']
     data_handle.store_tickers(db_path, chat_id, tickers)
